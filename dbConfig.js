cd6bc9845fd4b4fe0e2a2ab40b8ba060ff6716f2
Ryan <s10261024@connect.np.edu.sg>
2024-05-31 08:42:42 +0800

    add dbConfig

---
+++ dbConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    user: "", // Replace with your SQL Server login username
+    password: "", // Replace with your SQL Server login password
+    server: "localhost",
+    database: "",
+    trustServerCertificate: true,
+    options: {
+        port: 1433, // Default SQL Server port
+        connectionTimeout: 60000, // Connection timeout in milliseconds
+    },
+};
