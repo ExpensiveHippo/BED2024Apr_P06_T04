e1b649ea94e3c807599653395e181fb690a89c7a
ashuri <s10257765@connect.np.edu.sg>
2024-06-20 17:43:09 +0800

    completed logic for both Login User and Register User

--- dbConfig.js
+++ dbConfig.js
@@ -1,11 +1,11 @@
 module.exports = {
-    user: "", // Replace with your SQL Server login username
-    password: "", // Replace with your SQL Server login password
-    server: "localhost",
-    database: "",
-    trustServerCertificate: true,
-    options: {
-        port: 1433, // Default SQL Server port
-        connectionTimeout: 60000, // Connection timeout in milliseconds
-    },
-};
+    user:"testuser",
+    password:"test123",
+    server:"localhost",
+    database:"testDB",
+    trustServerCertificate:true,
+    options:{
+        port:1433,
+        connectionTimeout: 60000
+    }
+}
---
+++ public/JS/loginUser.js
@@ -0,0 +1,26 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            alert("You are now logged in!")
+            localStorage.setItem("username",username);
+            window.location.href = '/dashboard'; //enter next page if password is correct
+        } else {
+            document.getElementById('message').textContent = 'Invalid username or password';
+            setTimeout(() =>{
+                document.getElementById('message').textContent = '';
+            },5000);
+        }
+    });
+});
