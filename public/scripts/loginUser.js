53d7a2b6406846822aea6f0c7e33aecb0ab14d51
Ryan <s10261024@connect.np.edu.sg>
2024-06-30 13:01:45 +0800

    fix(login): change href

--- public/scripts/loginUser.js
+++ public/scripts/loginUser.js
@@ -16,7 +16,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
             alert("You are now logged in!")
             localStorage.setItem("username",username);
             localStorage.setItem("id", data.id);
-            window.location.href = '/dashboard'; //enter next page if password is correct
+            window.location.href = '/post.html'; //enter next page if password is correct
         } else {
             document.getElementById('message').textContent = 'Invalid username or password';
             setTimeout(() =>{
