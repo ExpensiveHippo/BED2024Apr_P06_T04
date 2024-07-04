0ed2e9e29ffc095b957b18a70f687318f5b50789
ashuri <s10257765@connect.np.edu.sg>
2024-07-04 18:28:32 +0800

    ensured that registering a user sets default role to 'user'

--- models/user.js
+++ models/user.js
@@ -2,11 +2,12 @@ const sql = require("mssql");
 const dbConfig = require("../dbConfig");
 
 class User{
-    constructor(id, username, email, password){
+    constructor(id, username, email, password, role = 'user'){
         this.id = id;
         this.username = username;
         this.email = email;
         this.password = password;
+        this.role = role;
     }
 
 
@@ -23,19 +24,21 @@ class User{
             result.recordset[0].id,
             result.recordset[0].username,
             result.recordset[0].email,
-            result.recordset[0].password
+            result.recordset[0].password,
+            result.recordset[0].role,
         )
         : null;
     } 
     static async createUser(newUserData){
         try{
             const connection = await sql.connect(dbConfig);
-            const sqlQuery = `INSERT INTO Users (username, email, password) VALUES(@username,@email,@password); SELECT SCOPE_IDENTITY() as id`
+            const sqlQuery = `INSERT INTO Users (username, email, password, role) VALUES(@username,@email,@password,@role); SELECT SCOPE_IDENTITY() as id`
 
             const request = connection.request();
             request.input('username',sql.VarChar,newUserData.username);
             request.input('email',sql.VarChar,newUserData.email);
             request.input('password',sql.VarChar,newUserData.password);
+            request.input('role',sql.VarChar,newUserData.role || 'user');
 
             const result = await request.query(sqlQuery);
             connection.close();
