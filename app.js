a08ed31e4f189910dc9608d551d3fce9c732adb6
ashuri <s10257765@connect.np.edu.sg>
2024-07-26 12:12:13 +0800

    Moved changes from main to 'ashton'

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const bodyParser = require('body-parser');
 const userController = require("./controllers/userController");
 const sql = require("mssql");
 const dbConfig = require("./dbConfig");
-const path = require('path');
+
+
+const userController = require("./controllers/userController");
+const postController = require("./controllers/postController");
+const likeController = require("./controllers/likeController");
+const commentController = require("./controllers/commentController");
+const authenticateToken = require('./middleware/authUser');
 
 const app = express();
 const port = 3000;
@@ -13,7 +19,16 @@ app.use(bodyParser.json());
 app.use(express.static('public')); // serve static files (HTML, CSS, JS)
 
 
-//Endpoint
+
+// Endpoints
+app.get("/Posts",postController.getAllPosts)
+app.get("/Posts/:postId",postController.getPostById)
+app.get("/like/:userId/:contentType/:contentId", likeController.getLike);
+app.get("/Comments",commentController.getAllComments)
+app.get("/Comments/:userId",commentController.getCommentsByUser)
+app.get("/Profile",authenticateToken,userController.getProfile);
+
+app.post("/createPost", postController.createPost);
 app.post('/login', userController.login);
 app.post('/register', userController.register);
 app.get('/dashboard', (req, res) => {
---
+++ middleware/authUser.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+const authenticateToken = (req,res,next) =>{
+    const token =
+    req.headers.authorization && req.headers.authorization.split(" ")[1]; // Authorization: "Bearer <TOKEN>"
+
+    if (!token){
+        return res.status(401).json({message:"Unauthorized"});
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,user) =>{
+        if (err) {
+            console.error(err);
+            return res.status(403).json({message: "Forbidden"});
+        }
+        const authorizedRoles = {
+            "/Profile": ["admin","user"],
+            // TODO: endpoints to be placed here for authorization of certain things
+        }
+
+        const requestedEndPoint = req.url; 
+        const userRole = user.role;
+        
+        const authorizedRole = Object.entries(authorizedRoles).find(
+            ([endpoint, roles]) => {
+              const regex = new RegExp(`^${endpoint}$`);
+              return regex.test(requestedEndPoint) && roles.includes(userRole);
+            }
+        )
+      
+        if (!authorizedRole) {
+        return res.status(403).json({ message: "Forbidden" });
+        }
+    
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = authenticateToken;
